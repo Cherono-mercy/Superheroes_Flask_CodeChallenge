413644299a17c61078a8994d46fbf1fba592d7d2
Cherono-mercy <cheronom142@gmail.com>
2024-01-16 09:34:04 +0800

    created GET/heroes route

--- app/app.py
+++ app/app.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, request, make_response, jsonify
+
+from flask_restful import Resource, Api
 from flask_migrate import Migrate
 
-from models import db, Hero
+from models import db, Hero, Power, HeroPower
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///heroes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app, db)
@@ -15,8 +18,22 @@ db.init_app(app)
 
 @app.route('/')
 def home():
-    return ''
+    return 'Welcome to my superheroes API'
+
+class Heroes(Resource):
+    def get(self):
+        heroes =[]
+        for hero in Hero.query.all():
+            hero_dict = {
+                "id": hero.id,
+                "name": hero.name,
+                "super_name": hero.super_name
+            }
+            heroes.append(hero_dict)
+        return make_response(jsonify(heroes), 200)  
+
+api.add_resource(Heroes, '/heroes')    
 
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=4000)
